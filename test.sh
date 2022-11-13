afbd9866a6b6e633ff85f4c0ad813105259166e7
Harris M. Snyder <harris.snyder@gmail.com>
2022-11-13 18:29:29 -0500

    added second example

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 cc -g lightir.c -o lightir.exe
-./lightir.exe as example.lightir > example.bin
-./lightir.exe run example.bin
+./lightir.exe as example-loop.lightir > example-loop.bin
+./lightir.exe as example-io.lightir > example-io.bin
